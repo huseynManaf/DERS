2abc2bfbe98d9c61e0f4d5c688b3dfa7ac6a06c8
huseynManaf <huseynim@code.edu.az>
2022-12-23 07:28:07 -0800

    init

---
+++ ConsoleApp39/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp39
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
